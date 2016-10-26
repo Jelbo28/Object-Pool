463f81d64447c545ea33360b869a4ee1280d3aec
Aram Joseph <ajoseph@utc4me.org>
2016-10-26 08:26:43 -0400

    Stuff

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField]
+    float m_speed = 0.1f;
+
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, m_speed) + new Vector3(0, 0, -1);
+        }
+    }
+}
---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour
+{
+    [SerializeField]
+    Text score;
+
+    float scoreInt = 0;
+
+    public static GM instance = null;
+
+    public void UpdateScore(float value)
+    {
+        scoreInt = scoreInt + value;
+        score.text = "Items Destroyed: " + scoreInt;
+    }
+}
